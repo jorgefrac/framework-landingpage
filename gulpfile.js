47f04b63cc12f1943e301d45eae8de390dde2a03
Jorge Fco <jorge_frac@hotmail.com>
2016-11-15 17:48:12 -0600

    task minify .js using gulp.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+
+/* =Plugins Gulp
+-----------------------------------------------------------------------------*/
+var uglify = require('gulp-uglify');
+var dest = require('gulp-dest');
+var jsmin = require('gulp-jsmin');
+var rename = require('gulp-rename');
+
+
+/* =Taks
+-----------------------------------------------------------------------------*/
+
+
+gulp.task('javascript', function() {
+	gulp.src([
+		'!js/jquery.min.js',
+		'js/*.js'
+	])
+	.pipe(uglify())
+	.pipe(jsmin())
+	.pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+});
